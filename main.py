b39697a35c3c28eaaf8640fd4458d145952d31d5
matheeshapathirana <pathiranamatheesha@gmail.com>
2023-10-08 21:25:07 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import json
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
+import os
 
+driver = webdriver.Firefox()
 def load_cookies_from_json(cookie_file_path):
     with open(cookie_file_path, 'r') as cookie_file:
         cookies = json.load(cookie_file)
@@ -23,20 +25,27 @@ def open_webpage_with_cookies(url, cookies):
 
     # Do further actions with the opened webpage
     if driver.find_elements(By.CSS_SELECTOR, '.btn'):
-        print(f"Cookie Not working - No. {num}")
+        print(f"Cookie Not working - {filename}")
         driver.quit()
     else:
-        print(f"Working cookie found! - No. {num}")
-        a = open(f'working_cookies/working_cookie ({num})','w')
-        a.write(cookie_file_path)
+        print(f"Working cookie found! - {filename}")
+        a = open(f'working_cookies/working_cookie ({filename})','w')
+        a.write(content)
         driver.quit()
 
-# Usage example
-num=1
-while (num != 195):
-    cookie_file_path = f'json_cookies/jsoncookie ({num}).txt'
+# switching cookies
+directory_path = 'json_cookies'
+for filename in os.listdir(directory_path):
+    filepath = os.path.join(directory_path, filename)
+    if os.path.isfile(filepath):
+        with open(filepath, 'r') as file:
+            content = file.read()
+
     url = 'https://netflix.com/login'
 
-    cookies = load_cookies_from_json(cookie_file_path)
-    open_webpage_with_cookies(url, cookies)
-    num+=1
+    try:
+        cookies = load_cookies_from_json(content)
+        open_webpage_with_cookies(url, cookies)
+    except:
+        driver.quit()
+        print(f"Invalid Cookie - {filename}")
