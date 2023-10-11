644f27f83c54d3bf1dc7561ea2ffb326e2eb6be8
matheeshapathirana <pathiranamatheesha@gmail.com>
2023-10-11 19:42:06 +0530

    Minor change

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # use True or False
 
 
-use_minimized = True  # minimize selenium browser
+use_minimized = False  # minimize selenium browser
 use_folder_selector = False  # use tkinter select folder window
