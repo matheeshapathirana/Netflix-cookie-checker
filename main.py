07ab72e6ca696d0bba761ca25666923e10b57510
matheeshapathirana <pathiranamatheesha@gmail.com>
2023-10-13 09:52:51 +0530

    Initial commit

--- main.py
+++ main.py
@@ -1,42 +1,39 @@
 import json
 import os
-import tkinter
-from tkinter import filedialog
-import config
+import time
+
+# import config
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.firefox.options import Options
 
 working_cookies_path = "working_cookies"
 
-if config.use_folder_selector:
-    tkinter.Tk().withdraw()
-    folder_path = filedialog.askdirectory()
-    if folder_path == "":
-        folder_path = "json_cookies"
-        print("Using default path")
-    else:
-        print(f"Using path: {folder_path}")
+folder_path = "json_cookies"
 
 
 def load_cookies_from_json(FILEPATH):
-    with open(FILEPATH, "r", encoding="utf-8") as cookie_file:
+    with open(FILEPATH, "r") as cookie_file:
         cookie = json.load(cookie_file)
+
     return cookie
 
 
 def open_webpage_with_cookies(URL, COOKIES):
-    firefox_options = Options()
-    firefox_options.add_argument("--headless")
-    driver = webdriver.Firefox(options=firefox_options)
-    if config.use_minimized:
-        driver.minimize_window()
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    # open it, go to a website, and get results
+    driver = webdriver.Chrome(options=options)
+
     driver.get(URL)
 
     for cookie in COOKIES:
         driver.add_cookie(cookie)
 
     driver.refresh()
+    print("we are trying")
+    driver.get_cookies
 
     if driver.find_elements(By.CSS_SELECTOR, ".btn"):
         print(f"Cookie Not working - {filename}")
@@ -45,12 +42,12 @@ def open_webpage_with_cookies(URL, COOKIES):
         print(f"Working cookie found! - {filename}")
         try:
             os.mkdir(working_cookies_path)
-            with open(f"working_cookies/{filename})", "w", encoding="utf-8") as a:
+            with open(f"working_cookies/{filename})", "w") as a:
                 a.write(content)
             driver.quit()
 
-        except FileExistsError:
-            with open(f"working_cookies/{filename}", "w", encoding="utf-8") as a:
+        except:
+            with open(f"working_cookies/{filename}", "w") as a:
                 a.write(content)
             driver.quit()
 
@@ -58,13 +55,15 @@ def open_webpage_with_cookies(URL, COOKIES):
 for filename in os.listdir("json_cookies"):
     filepath = os.path.join("json_cookies", filename)
     if os.path.isfile(filepath):
-        with open(filepath, "r", encoding="utf-8") as file:
+        with open(filepath, "r") as file:
             content = file.read()
 
             url = "https://netflix.com/login"
 
             try:
+                print("We are trying....")
                 cookies = load_cookies_from_json(filepath)
                 open_webpage_with_cookies(url, cookies)
-            except Exception as e:
-                print(f"Error occurred: {str(e)} - {filename}")
+            except:
+                print(f"Invalid Cookie or webdriver error - {filename}")
+                time.sleep(2)
