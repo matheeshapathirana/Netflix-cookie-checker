b221f226951034a0f928ca41c83d0b8cd24cf804
matheeshapathirana <pathiranamatheesha@gmail.com>
2023-10-13 09:41:51 +0530

    Fixed headless mode

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def load_cookies_from_json(FILEPATH):
 
 def open_webpage_with_cookies(URL, COOKIES):
     firefox_options = Options()
-    firefox_options.headless = True
+    firefox_options.add_argument("--headless")
     driver = webdriver.Firefox(options=firefox_options)
     if config.use_minimized:
         driver.minimize_window()
