4ac14e7d7e2545607d4ac3e82cad380ad6972a65
matheeshapathirana <pathiranamatheesha@gmail.com>
2023-10-13 11:11:48 +0530

    Exception handling

--- README.md
+++ README.md
@@ -17,11 +17,13 @@
 ```
 # Usage
 
-1. Upload cookies (Netscape format) to `Netflix-cookie-checker/cookies` folder
+1. Upload cookies (Netscape format) to `Netflix-cookie-checker/cookies` folder.
+
+   ***NOTE :*** If you already have json cookies then create a folder named `json_cookies` and upload cookies to that folder then run [main.py](https://github.com/matheeshapathirana/Netflix-cookie-checker/blob/0b04b55e3b1d9bdc35f955d926e794851280d4d0/main.py)
 2. Run [cookie_converter.py](https://github.com/matheeshapathirana/Netflix-cookie-checker/blob/b82b684355a80e23f5648e6082090d9cd5332cc3/cookie_converter.py) to convert Netscape cookies to json format.
 
    `!python3 cookie_converter.py`
-3. Run [main.py](https://github.com/matheeshapathirana/Netflix-cookie-checker/blob/b82b684355a80e23f5648e6082090d9cd5332cc3/main.py).
+3. Run [main.py](https://github.com/matheeshapathirana/Netflix-cookie-checker/blob/0b04b55e3b1d9bdc35f955d926e794851280d4d0/main.py).
 
     `!python3 main.py`
 # For any issues
--- main.py
+++ main.py
@@ -42,17 +42,20 @@ def open_webpage_with_cookies(URL, COOKIES):
                 a.write(content)
             driver.quit()
 
+try:
+    for filename in os.listdir("json_cookies"):
+        filepath = os.path.join("json_cookies", filename)
+        if os.path.isfile(filepath):
+            with open(filepath, "r") as file:
+                content = file.read()
 
-for filename in os.listdir("json_cookies"):
-    filepath = os.path.join("json_cookies", filename)
-    if os.path.isfile(filepath):
-        with open(filepath, "r") as file:
-            content = file.read()
+                url = "https://netflix.com/login"
 
-            url = "https://netflix.com/login"
+                try:
+                    cookies = load_cookies_from_json(filepath)
+                    open_webpage_with_cookies(url, cookies)
+                except Exception as e:
+                    print(f"Error occurred {str(e)} - {filename}")
 
-            try:
-                cookies = load_cookies_from_json(filepath)
-                open_webpage_with_cookies(url, cookies)
-            except Exception as e:
-                print(f"Error occurred {str(e)} - {filename}")
+except FileNotFoundError:
+    print("Error occurred : 'json_cookies' folder not found!. Please read README.nd")
