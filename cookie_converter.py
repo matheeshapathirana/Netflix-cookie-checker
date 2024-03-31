42df6e5eafc30aa2139cd744b3cb160683075224
matheeshapathirana <matheesha.adithya.pathirana@gmail.com>
2024-03-31 17:59:26 +0530

    Minor Update

--- cookie_converter.py
+++ cookie_converter.py
@@ -3,7 +3,9 @@ import os
 import random
 import shutil
 import sys
+from colorama import Fore
 
+no_of_cookies = 0
 
 try:
     if os.name == "posix":
@@ -14,30 +16,25 @@ try:
             import tkinter
             from tkinter import filedialog
 
-            print("\n<<< Select Netscape cookies folder >>>\n\n")
+            print(Fore.YELLOW + "\n<<< Select Netscape cookies folder >>>\n\n" + Fore.RESET)
             tkinter.Tk().withdraw()
             folder_path = filedialog.askdirectory()
             if folder_path == "":
                 if os.path.isdir("cookies"):
                     folder_path = "cookies"
-                    print("Trying to use default folder 'cookies'\n")
+                    print(Fore.YELLOW + "Trying to use default folder 'cookies'\n" + Fore.RESET)
                     break
                 else:
-                    print(
-                        "No folder selected or default 'cookies' folder not found, Exiting..."
-                    )
+                    print(Fore.RED +
+                          "[⚠️] No folder selected or default 'cookies' folder not found, Exiting..."
+                          + Fore.RESET)
                     sys.exit()
 
             else:
                 break
 
-    def maximum():
-        count = 0
-        for files in os.walk(folder_path):
-            count += len(files)
-            return count
-
     rand_number = random.randint(1, 99999)
+
 
     def convert_netscape_cookie_to_json(cookie_file_content):
         cookies = []
@@ -58,10 +55,11 @@ try:
         json_content = json.dumps(cookies, indent=4)
         return json_content
 
+
     path = "json_cookies"
     try:
         os.mkdir(path)
-        print(f"Folder {path} created!\n")
+        print(Fore.RED + f"Folder {path} created!\n" + Fore.RESET)
         try:
             for filename in os.listdir(folder_path):
                 filepath = os.path.join(folder_path, filename)
@@ -73,19 +71,21 @@ try:
 
                     with open(f"json_cookies/{filename}", "w", encoding="utf-8") as f:
                         f.write(json_data)
-                        print(f"{filename} - DONE!")
+                        print(Fore.GREEN + f"[✔️] {filename} - DONE!" + Fore.RESET)
+                        no_of_cookies += 1
+
         except FileNotFoundError:
-            print(
-                "Error Occurred :Default 'cookies' folder not found, please select a valid folder"
-            )
+            print(Fore.RED +
+                  "[⚠️] Error Occurred: Default 'cookies' folder not found, please select a valid folder"
+                  + Fore.RESET)
             os.rmdir(path)
 
     except FileExistsError:
         if (
-            input(
-                "Do you want to remove old cookies folder? (y/n)\n [y] Recommended \n > : "
-            )
-            == "y"
+                input(Fore.YELLOW +
+                      "Do you want to remove old cookies folder? (y/n)\n [y] Recommended \n [n] New cookies will be appended > : "
+                      + Fore.RESET)
+                == "y"
         ):
             shutil.rmtree(path)
             os.mkdir(path)
@@ -99,10 +99,10 @@ try:
 
                     with open(f"json_cookies/{filename}", "w", encoding="utf-8") as f:
                         f.write(json_data)
-                        print(f"{filename} - DONE!")
+                        print(Fore.GREEN + f"[✔️] {filename} - DONE!" + Fore.RESET)
+                        no_of_cookies += 1
 
         else:
-            os.mkdir(str(f"temp {rand_number}"))
             for filename in os.listdir(folder_path):
                 filepath = os.path.join(folder_path, filename)
                 if os.path.isfile(filepath):
@@ -112,13 +112,13 @@ try:
                     json_data = convert_netscape_cookie_to_json(content)
 
                     with open(
-                        f"temp {rand_number}/{filename}", "w", encoding="utf-8"
+                            f"json_cookies/{filename}", "w", encoding="utf-8"
                     ) as f:
                         f.write(json_data)
-                        print(f"{filename} - DONE!")
+                        print(Fore.GREEN + f"[✔️] {filename} - DONE!" + Fore.RESET)
+                        no_of_cookies += 1
 
-            print(f"\n\nsaved cookies to the temp folder - temp {rand_number}")
-
+    print(Fore.YELLOW+f"\nConverted {no_of_cookies} cookies to JSON format\n"+Fore.RESET)
 except KeyboardInterrupt:
-    print("\n\nProgram Interrupted by user")
+    print(Fore.RED + "\n\nProgram Interrupted by user" + Fore.RESET)
     sys.exit()
