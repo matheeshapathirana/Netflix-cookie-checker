208124a1d0c984d75c76a21dc0b8cb9ce0d4ac89
matheeshapathirana <matheesha.adithya.pathirana@gmail.com>
2024-04-17 19:25:35 +0530

    Fixed #64

--- assets/cookie_converter.py
+++ assets/cookie_converter.py
@@ -1,89 +1,102 @@
 import json
 import os
-import random
-import tkinter
-from tkinter import filedialog
 import shutil
+import sys
+from colorama import Fore
 
-while True:
-    print("\n<<< Select Netscape cookies folder >>>\n\n")
-    tkinter.Tk().withdraw()
-    folder_path = filedialog.askdirectory()
-    if folder_path != "":
-        break
+no_of_cookies = 0
 
-rand_number = random.randint(1, 99999)
-
-
-def convert_netscape_cookie_to_json(cookie_file_content):
-    cookies = []
-    for line in cookie_file_content.splitlines():
-        fields = line.strip().split("\t")
-        if len(fields) >= 7:
-            cookie = {
-                "domain": fields[0],
-                "flag": fields[1],
-                "path": fields[2],
-                "secure": fields[3] == "TRUE",
-                "expiration": fields[4],
-                "name": fields[5],
-                "value": fields[6],
-            }
-            cookies.append(cookie)
-
-    JSON_DATA = json.dumps(cookies, indent=4)
-    return JSON_DATA
-
-
-path = "json_cookies"
 try:
-    os.mkdir(path)
-    print(f"Folder {path} created!")
-    for filename in os.listdir(folder_path):
-        filepath = os.path.join(folder_path, filename)
-        if os.path.isfile(filepath):
-            with open(filepath, "r", encoding="utf-8") as file:
-                content = file.read()
+    folder_path = "cookies"
 
-            json_data = convert_netscape_cookie_to_json(content)
+    def convert_netscape_cookie_to_json(cookie_file_content):
+        cookies = []
+        for line in cookie_file_content.splitlines():
+            fields = line.strip().split("\t")
+            if len(fields) >= 7:
+                cookie = {
+                    "domain": fields[0].replace("www", ""),
+                    "flag": fields[1],
+                    "path": fields[2],
+                    "secure": fields[3] == "TRUE",
+                    "expiration": fields[4],
+                    "name": fields[5],
+                    "value": fields[6],
+                }
+                cookies.append(cookie)
 
-            with open(f"json_cookies/{filename}", "w", encoding="utf-8") as f:
-                f.write(json_data)
-                print(f"{filename} - DONE!")
+        json_content = json.dumps(cookies, indent=4)
+        return json_content
 
-except FileExistsError:
-    if (
-        input(
-            "Do you want to remove old cookies folder? (y/n)\n [y] Recommended \n > : "
-        )
-        == "y"
-    ):
-        shutil.rmtree(path)
+    path = "json_cookies"
+    try:
         os.mkdir(path)
-        for filename in os.listdir(folder_path):
-            filepath = os.path.join(folder_path, filename)
-            if os.path.isfile(filepath):
-                with open(filepath, "r", encoding="utf-8") as file:
-                    content = file.read()
+        print(Fore.RED + f"Folder {path} created!\n" + Fore.RESET)
+        try:
+            for filename in os.listdir(folder_path):
+                filepath = os.path.join(folder_path, filename)
+                if os.path.isfile(filepath):
+                    with open(filepath, "r", encoding="utf-8") as file:
+                        content = file.read()
 
-                json_data = convert_netscape_cookie_to_json(content)
+                    json_data = convert_netscape_cookie_to_json(content)
 
-                with open(f"json_cookies/{filename}", "w", encoding="utf-8") as f:
-                    f.write(json_data)
-                    print(f"{filename} - DONE!")
+                    with open(f"json_cookies/{filename}", "w", encoding="utf-8") as f:
+                        f.write(json_data)
+                        print(Fore.GREEN + f"[✔️] {filename} - DONE!" + Fore.RESET)
+                        no_of_cookies += 1
 
-    else:
-        os.mkdir(str(f"temp {rand_number}"))
-        for filename in os.listdir(folder_path):
-            filepath = os.path.join(folder_path, filename)
-            if os.path.isfile(filepath):
-                with open(filepath, "r") as file:
-                    content = file.read()
+        except FileNotFoundError:
+            print(
+                Fore.RED
+                + "[⚠️] Error Occurred: Default 'cookies' folder not found, please select a valid folder"
+                + Fore.RESET
+            )
+            os.rmdir(path)
 
-                json_data = convert_netscape_cookie_to_json(content)
+    except FileExistsError:
+        if (
+            input(
+                Fore.YELLOW
+                + "Do you want to remove old cookies folder? (y/n)\n [y] Recommended \n [n] New cookies will be appended > : "
+                + Fore.RESET
+            )
+            == "y"
+        ):
+            shutil.rmtree(path)
+            os.mkdir(path)
+            for filename in os.listdir(folder_path):
+                filepath = os.path.join(folder_path, filename)
+                if os.path.isfile(filepath):
+                    with open(filepath, "r", encoding="utf-8") as file:
+                        content = file.read()
 
-                with open(f"temp {rand_number}/{filename}", "w", encoding="utf-8") as f:
-                    f.write(json_data)
-                    print(f"{filename} - DONE!")
+                    json_data = convert_netscape_cookie_to_json(content)
 
-        print(f"\n\nsaved cookies to the temp folder - temp {rand_number}")
+                    with open(f"json_cookies/{filename}", "w", encoding="utf-8") as f:
+                        f.write(json_data)
+                        print(Fore.GREEN + f"[✔️] {filename} - DONE!" + Fore.RESET)
+                        no_of_cookies += 1
+
+        else:
+            for filename in os.listdir(folder_path):
+                filepath = os.path.join(folder_path, filename)
+                if os.path.isfile(filepath):
+                    with open(filepath, "r") as file:
+                        content = file.read()
+
+                    json_data = convert_netscape_cookie_to_json(content)
+
+                    with open(f"json_cookies/{filename}", "w", encoding="utf-8") as f:
+                        f.write(json_data)
+                        print(Fore.GREEN + f"[✔️] {filename} - DONE!" + Fore.RESET)
+                        no_of_cookies += 1
+
+    print(
+        Fore.YELLOW
+        + f"\nConverted {no_of_cookies} cookies to JSON format\n"
+        + Fore.RESET
+    )
+except KeyboardInterrupt:
+    print(Fore.RED + "\n\nProgram Interrupted by user" + Fore.RESET)
+    sys.exit()
--- assets/main.py
+++ assets/main.py
@@ -1,70 +1,178 @@
 import json
 import os
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+import sys
+import asyncio
+import time
+import aiohttp
+from bs4 import BeautifulSoup
+from colorama import Fore
 
+print(Fore.YELLOW + "Initializing!, Please wait...\n" + Fore.RESET)
 working_cookies_path = "working_cookies"
+exceptions = 0
+working_cookies = 0
+expired_cookies = 0
+start = time.time()
+plan = None
+
+num_threads = 40  # <--- Define the number of threads here
+
+# ___________________________________________
+# | Network Speed | Recommended no. threads |
+# |---------------|-------------------------|
+# | < 5 Mbps      | 1-3                     |
+# | 5-20 Mbps     | 3-5                     |
+# | 20-100 Mbps   | 5-10                    |
+# | > 100 Mbps    | 10-20                   |
+# |_________________________________________|
 
 
-def load_cookies_from_json(FILEPATH):
-    with open(FILEPATH, "r") as cookie_file:
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Referer": "https://www.netflix.com/login",
+    "DNT": "1",
+    "Sec-GPC": "1",
+    "Connection": "keep-alive",
+    "Upgrade-Insecure-Requests": "1",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "same-origin",
+    "Sec-Fetch-User": "?1",
+}
+
+
+async def load_cookies_from_json(json_cookies_path):
+    with open(json_cookies_path, "r", encoding="utf-8") as cookie_file:
         cookie = json.load(cookie_file)
     return cookie
 
 
-def open_webpage_with_cookies(URL, COOKIES):
-    options = webdriver.ChromeOptions()
-    options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    driver = webdriver.Chrome(options=options)
+async def open_webpage_with_cookies(session, link, json_cookies, filename):
+    global working_cookies, expired_cookies, plan
 
-    driver.get(URL)
+    # Request the page
+    await session.get(link)
 
-    for cookie in COOKIES:
-        driver.add_cookie(cookie)
-    driver.refresh()
-    driver.get_cookies()
+    # Clear all existing cookies
+    session.cookie_jar.clear()
 
-    if driver.find_elements(By.CSS_SELECTOR, ".btn") or driver.find_elements(
-            By.CSS_SELECTOR, ".e1ax5wel1"):
-        print(f"Cookie Not working - {filename}")
-        driver.quit()
-    else:
-        print(f"Working cookie found! - {filename}")
+    for cookie in json_cookies:
+        session.cookie_jar.update_cookies({cookie["name"]: cookie["value"]})
+
+    async with session.get(link, headers=headers, timeout=10) as response:
+        content = await response.text()
+        soup = BeautifulSoup(content, "lxml")
+        if soup.find(string="Sign In") or soup.find(string="Sign in"):
+            print(Fore.RED + f"[❌] Cookie Not working - {filename}" + Fore.RESET)
+            expired_cookies += 1
+        else:
+            plan = (
+                "Premium"
+                if soup.find(string="Premium")
+                else (
+                    "Basic"
+                    if soup.find(string="Basic")
+                    else "Standard" if soup.find(string="Standard") else "Unknown"
+                )
+            )
+            print(
+                Fore.GREEN
+                + f"[✔️] Cookie Working - {filename} | Plan: {plan}"
+                + Fore.RESET
+            )
+            try:
+                os.mkdir(working_cookies_path)
+                working_cookies += 1
+                return content  # Return content if the cookie is working
+            except FileExistsError:
+                working_cookies += 1
+                return content  # Return content if the cookie is working
+
+
+async def process_cookie_file(filename):
+    filepath = os.path.join("json_cookies", filename)
+    if os.path.isfile(filepath):
+        with open(filepath, "r", encoding="utf-8"):
+            url = "https://www.netflix.com/YourAccount"
+            try:
+                cookies = await load_cookies_from_json(filepath)
+                async with aiohttp.ClientSession() as session:
+                    content = await open_webpage_with_cookies(
+                        session, url, cookies, filename
+                    )
+                    if content:
+                        # Save working cookies to JSON file
+                        with open(
+                            f"working_cookies/{filename} - {plan}.json", "w"
+                        ) as json_file:
+                            json.dump(cookies, json_file)
+            except json.decoder.JSONDecodeError:
+                print(
+                    Fore.RED
+                    + f"[⚠️] Please use cookie_converter.py to convert your cookies to json format! (File: {filename})\n"
+                    + Fore.RESET
+                )
+                global exceptions
+                exceptions += 1
+            except Exception as e:
+                print(
+                    Fore.RED
+                    + f"[⚠️] Error occurred: {str(e)} - {filename}\n"
+                    + Fore.RESET
+                )
+                exceptions += 1
+
+
+async def main():
+    try:
+        os.path.isdir("json_cookies")
         try:
-            os.mkdir(working_cookies_path)
-            with open(f"working_cookies/{filename})", "w") as a:
-                a.write(content)
-            driver.quit()
+            if os.path.isdir("working_cookies"):
+                print(
+                    Fore.RED
+                    + "[⚠️] working_cookies folder already exists, new cookies will be appended.\n"
+                    + Fore.RESET
+                )
 
-        except FileExistsError:
-            with open(f"working_cookies/{filename}", "w") as a:
-                a.write(content)
-            driver.quit()
+            tasks = []
+            for filename in os.listdir("json_cookies"):
+                task = asyncio.create_task(process_cookie_file(filename))
+                tasks.append(task)
+                if len(tasks) >= num_threads:
+                    await asyncio.gather(*tasks)
+                    tasks = []
+            if tasks:
+                await asyncio.gather(*tasks)
+        except FileNotFoundError:
+            tasks = []
+            for filename in os.listdir("json_cookies"):
+                task = asyncio.create_task(process_cookie_file(filename))
+                tasks.append(task)
+                if len(tasks) >= num_threads:
+                    await asyncio.gather(*tasks)
+                    tasks = []
+            if tasks:
+                await asyncio.gather(*tasks)
+    except FileNotFoundError:
+        print(
+            Fore.RED
+            + "[⚠️] Error Occurred: Please use cookie_converter.py to convert cookies."
+            + Fore.RESET
+        )
+        sys.exit()
 
 
 try:
-    for filename in os.listdir("json_cookies"):
-        filepath = os.path.join("json_cookies", filename)
-        if os.path.isfile(filepath):
-            with open(filepath, "r") as file:
-                content = file.read()
-
-                url = "https://netflix.com/login"
-
-            try:
-                cookies = load_cookies_from_json(filepath)
-                open_webpage_with_cookies(url, cookies)
-
-            except json.decoder.JSONDecodeError:
-                print(
-                    "Please use cookie_converter.py to convert your cookies to json format\n"
-                )
-                break
-
-            except Exception as e:
-                print(f"Error occurred: {str(e)} - {filename}\n")
-
-except FileNotFoundError:
-    print("Error occurred : 'json_cookies' folder not found!. Please read README.md")
+    asyncio.run(main())
+    end = time.time()
+    print(
+        Fore.YELLOW
+        + f"\nSummary:\nTotal cookies: {len(os.listdir('json_cookies'))}\nWorking cookies: {working_cookies}\nExpired cookies: {expired_cookies}\nInvalid cookies: {exceptions}\nTime Elapsed: {round((end - start))} Seconds"
+        + Fore.RESET
+    )
+except KeyboardInterrupt:
+    print(Fore.RED + "\n\nProgram Interrupted by user" + Fore.RESET)
+    sys.exit()
